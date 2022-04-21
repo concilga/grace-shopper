cb44f563d7468fc0f74cbb84f6763bf59024c831
Colton G <c.gazda21@yahoo.com>
2022-04-21 18:40:33 -0400

    ok

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
 import { useState } from "react";
 import { useEffect } from "react";
-
+import { Route } from "react-router-dom";
 const App = () => {
-  const [products, setProducts] = useState([]);
-
-  const fetchProducts = async () => {
-    const response = await fetch("/api/products");
+  const [token, setToken] = useState("");
+  const [user, setUser] = useState(null);
+  const [beers, setBeer] = useState([]);
+  const fetchUser = async () => {
+    const isToken = localStorage.getItem("token");
+    if (isToken) {
+      setToken(isToken);
+    }
+    const response = await fetch("/api/users/me", {
+      headers: {
+        Authorization: `Bearer ${isToken}`,
+      },
+    });
     const info = await response.json();
-    setProducts(info);
+    if (info.id) {
+      setUser(info);
+    }
+  };
+  const fetchBeer = async () => {
+    const response = await fetch("/api/beer", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const info = await response.json();
+    setBeer(info);
   };
   useEffect(() => {
-    fetchProducts();
+    fetchUser();
+    fetchBeer();
   }, []);
-
-  return <>Full-Stack App!!!!</>;
+  return (
+    <>
+      <Navbar />
+      <Route exact path="/" element={<Home />} />
+      <Route path="/Beer" element={<Beer beers={beers} />} />
+      <Route path="/Account" element={<Account />} />
+      <Route
+        path="/Login"
+        element={<Login token={token} setToken={setToken} />}
+      />
+      <Route
+        path="/Register"
+        element={<Register token={token} setToken={setToken} />}
+      />
+      <Route path="/Cart" element={<Cart />} />
+    </>
+  );
 };
-
 export default App;
