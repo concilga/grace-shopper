77d489610af569424850450364bcd6500389abc9
orpi3s <92934985+orpi3s@users.noreply.github.com>
2022-04-23 10:52:35 -0400

    Minor changes to HomeP and Draft Cart

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { useHistory, useParams, Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 const Home = () => {
   return (
-    <div className="home-page">
-      <h1 id="home-title">Home</h1>
-      <img scr="/images/My project (16).png" alt="none" width="500" height="500"/>
+    <div className="homePage">
+      <h1 id="home-title"> Hello </h1>
+
+      {/* <img
+        scr="/images/My project (16).png"
+        alt="none"
+        width="500"
+        height="500"
+      /> */}
       {/* <h1 id="logo">:runner:</h1> */}
       {/* <h1>All your fitness needs in one place!</h1> */}
     </div>
