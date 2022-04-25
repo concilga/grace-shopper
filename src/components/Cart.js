c0f09d4a0632b52b0da877fe7c2dc9c942b14fab
orpi3s <92934985+orpi3s@users.noreply.github.com>
2022-04-25 18:26:25 -0400

    Changes for Carts and Beer JS

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,38 +1,87 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { getUserOpenCart } from "../../db/cart";
+import { useState } from "react";
+
 const Cart = () => {
-  const {
-    items,
-    totalUniqueItems,
-    totalItems,
-    updateItemQuanity,
-    removeItem,
-    emptyCart,
-  } = getUserOpenCart();
+  const [userCart, setUserCart] = useState([]);
+
+  async function fetchCartBeers() {
+    const response = await fetch("/api/cart_beers/", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const info = await response.json();
+    console.log(info, "info test");
+    if (info.name) {
+      console.log(info.name);
+    }
+
+    setUserCart(info);
+  }
+
+  useEffect(() => {
+    fetchCartBeers();
+  }, [token]);
+
+  //Use the Cart.js via api for the requests on buttons.
+
+  async function removeBeerFromCart(beerId, userId) {
+    const response = await fetch(`/api/cart_beers/${(beerId, userId)}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        userId: userId,
+        beerId: beerId,
+      }),
+    });
+    const info = await response.json();
+    console.log(info);
+  }
+  //Also helper functions and console logs to test.
+
+  async function updateItemQuanity(beerId, quanity) {
+    const response = await fetch(`/api/cart_beers/${beerId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        quanity: quanity,
+      }),
+    });
+    const info = await response.json();
+    console.log(info);
+  }
+
   if (isEmpty) return <h1 className="cart-text">Your Cart is Empty</h1>;
+  //fetch requesti
+
   return (
     <section className="cart-container">
       <div className="COL-3">
-        <h5>
-          Cart ({totalUniqueItems}) total Items: ({totalItems}){" "}
-        </h5>
+        <h5>Cart Total Items: ({quanity}) </h5>
         <table className="table">
           <tbody>
-            {items.map((item, index) => {
+            {beerId.map((userId) => {
               return (
-                <tr key={index}>
+                <tr key={userId}>
                   <td>
-                    <img src={item.image} style={{ height: "6rem" }} />
+                    <img src={beerId.image} style={{ height: "6rem" }} />
                   </td>
-                  <td>{item.name}</td>
-                  <td>{item.price}</td>
-                  <td>Quantity ({item.quanity})</td>
+                  <td>{beerId.name}</td>
+                  <td>{beerId.price}</td>
+                  <td>Quantity ({beerId.quanity})</td>
                   <td>
                     <button
                       className="btn-update-quanity"
                       onClick={() =>
-                        updateItemQuanity(item.id, item.quanity + 1)
+                        updateItemQuanity(beerId.id, beerId.quanity + 1)
                       }
                     >
                       +
@@ -40,14 +89,14 @@ const Cart = () => {
                     <button
                       className="btn-update-quanity"
                       onClick={() =>
-                        updateItemQuanity(item.id, item.quanity - 1)
+                        updateItemQuanity(beerId.id, beerId.quanity - 1)
                       }
                     >
                       -
                     </button>
                     <button
                       className="btn-remove-item"
-                      onClick={() => removeItem(item.id)}
+                      onClick={() => removeBeerFromCart(beerId.id)}
                     >
                       Remove Item
                     </button>
@@ -59,7 +108,7 @@ const Cart = () => {
         </table>
       </div>
       <div className="cart-total-price">
-        <h2>Total Price ${cartTotal}</h2>
+        <h2>Total Price ${price}</h2>
       </div>
       <div className="clear-Cart" onClick={() => emptyCart()}>
         Clear Cart
