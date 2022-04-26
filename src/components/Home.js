e8d47f4383cf0a55723ac75b3bcb2c61e89199f2
orpi3s <92934985+orpi3s@users.noreply.github.com>
2022-04-25 22:10:30 -0400

    Changes To HomePage and CSS

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,18 +2,19 @@ import { useHistory, useParams, Link } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 const Home = () => {
   return (
-    <div className="homePage">
-      <h1 id="home-title"> Hello </h1>
-
-      {/* <img
-        scr="/images/My project (16).png"
-        alt="none"
-        width="500"
-        height="500"
-      /> */}
-      {/* <h1 id="logo">:runner:</h1> */}
-      {/* <h1>All your fitness needs in one place!</h1> */}
-    </div>
+    <>
+      <div class="logo">
+        <img src="HPL.jpg" />
+      </div>
+      <div className="homePage">
+        <h1>
+          We are <span>4 STAR</span>
+          <span> Beers</span>
+        </h1>
+        <Link to="/Beer">Explore</Link>
+        {/* <a href="#">Browse Our Selection</a> */}
+      </div>
+    </>
   );
 };
 export default Home;
